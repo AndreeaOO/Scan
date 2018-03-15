fee786c2b726be26c3541ea376f2d1e735574200
Henrik Bulskov <bulskov@ruc.dk>
2018-03-15 11:08:20 +0100

    First EF example

---
+++ EfExample/NorthwindContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfExample
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseMySql(
+                "server=localhost;database=northwind;uid=bulskov;pwd=henrik;SslMode=none");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+
+        }
+    }
+}
---
+++ EfExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EfExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new NorthwindContext())
+            {
+                foreach (var category in db.Categories)
+                {
+                    Console.WriteLine(category.Name);
+                }
+            }
+        }
+    }
+}
